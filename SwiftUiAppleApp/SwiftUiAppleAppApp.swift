ae150e6ba7798427100702490e1cc56ec631c34a
devbrunolopes <brunolopes0203@gmail.com>
2023-11-26 08:42:24 -0300

    first commit

---
+++ SwiftUiAppleApp/SwiftUiAppleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiAppleAppApp.swift
+//  SwiftUiAppleApp
+//
+//  Created by Bruno Lopes on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiAppleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
