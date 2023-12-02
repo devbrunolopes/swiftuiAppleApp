3ed8f45a02e0b30bd6172c0051e6dd6bb578d3de
devbrunolopes <brunolopes0203@gmail.com>
2023-12-02 10:06:22 -0300

    creates layout

---
+++ SwiftUiAppleApp/Views/DetailView.swift
@@ -0,0 +1,46 @@
+//
+//  DetailView.swift
+//  SwiftUiAppleApp
+//
+//  Created by Bruno Lopes on 02/12/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum:  DailyScrum
+    
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                Label("Start Meeting", systemImage: "timer")
+                    .foregroundColor(.accentColor)
+                    .font(.headline)
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
+            }
+        }
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
+}
