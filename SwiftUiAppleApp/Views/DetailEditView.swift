456d2e2307ee73c547a5420c1f009ff8b45a6040
devbrunolopes <brunolopes0203@gmail.com>
2023-12-16 07:53:10 -0300

    creates logic to add a newattendee to a meeting

--- SwiftUiAppleApp/Views/DetailEditView.swift
+++ SwiftUiAppleApp/Views/DetailEditView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct DetailEditView: View {
     @State private var scrum = DailyScrum.emptyScrum
+    @State private var newAttendeeName = ""
     
     var body: some View {
         Form {
@@ -28,6 +29,19 @@ struct DetailEditView: View {
                 }
                 .onDelete { indices in
                     scrum.attendees.remove(atOffsets: indices)
+                }
+                HStack {
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button(action: {
+                        withAnimation {
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                            scrum.attendees.append(attendee)
+                            newAttendeeName = ""
+                        }
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    .disabled(newAttendeeName.isEmpty)
                 }
             }
         }
